a0bc02fe76bee672aeaf11f5aac5147bc5d92f3d
Zainul <zainul1996@gmail.com>
2024-03-19 06:30:08 +0800

    fixed build error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from "@vercel/analytics/react"
