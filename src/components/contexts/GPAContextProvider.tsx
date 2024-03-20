6f5eee0dc15fab9832edb71701f780e07631093d
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-20 20:53:48 +0800

    feat: update GPAContextProvider to export editModule and Target GPA and Target Credits.
    Also, update setSchool exported fn to reset modules everytime the school is changed.

--- src/components/contexts/GPAContextProvider.tsx
+++ src/components/contexts/GPAContextProvider.tsx
@@ -1,4 +1,9 @@
-import React, { createContext, PropsWithChildren, useContext, useState } from 'react';
+import React, {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useState,
+} from 'react';
 import { Module } from '@utils/gpaCalculator';
 import GPAData from '@utils/GPAData';
 
@@ -14,13 +19,16 @@ interface GPAContextInterface {
   setSchool: (school: string | null) => void;
   setCurrentGPA: (gpa: number | null) => void;
   setCurrentCredits: (credits: number | null) => void;
+  setTargetGPA: (gpa: number | null) => void;
+  setTargetCredits: (credits: number | null) => void;
 
   addModule: (module: Module) => void;
+  editModule: (index: number, module: Module) => void;
   removeModule: (index: number) => void;
 }
 
-type Schools = typeof GPAData[number]['school']
-export type School = typeof GPAData[number]
+type Schools = (typeof GPAData)[number]['school'];
+export type School = (typeof GPAData)[number];
 
 export const GPAContext = createContext<GPAContextInterface | null>(null);
 
@@ -33,16 +41,16 @@ export const useGPAContext = () => {
   return items;
 };
 
-
 /**
  * GPA Context Provider stores logic of the application and also some helper functions for the states
  * @param children
  * @constructor
  */
-export const GPAContextProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
-
+export const GPAContextProvider: React.FC<PropsWithChildren<{}>> = ({
+  children,
+}) => {
   // Used for CGPA calculation
-  const [school, setSchool] = useState<string | null>(null);
+  const [school, _setSchool] = useState<string | null>(null);
   const [currentGPA, setCurrentGPA] = useState<number | null>(null);
   const [currentCredits, setCurrentCredits] = useState<number | null>(null);
   const [modules, setModules] = useState<Module[]>([]);
@@ -51,6 +59,19 @@ export const GPAContextProvider: React.FC<PropsWithChildren<{}>> = ({ children }
   const [targetGPA, setTargetGPA] = useState<number | null>(null);
   const [targetCredits, setTargetCredits] = useState<number | null>(null);
 
+  /**
+   * Set the school state
+   * The reason for this is to reset the modules when the school is changed
+   * This is because different schools have different grading systems
+   * @param name
+   */
+  const setSchool = (name: string | null) => {
+    if (!name) return;
+
+    _setSchool(name);
+    setModules([]);
+  };
+
   const addModule = (module: Module) => {
     setModules((prev) => {
       return [...prev, { ...module }];
@@ -58,30 +79,46 @@ export const GPAContextProvider: React.FC<PropsWithChildren<{}>> = ({ children }
   };
 
   const removeModule = (index: number) => {
-    setModules(prev => {
+    setModules((prev) => {
       return prev.filter((_, i) => i !== index);
     });
   };
 
-  console.log('[DEBUG - GPACONTEXTPROVIDER] school', school);
-  console.log('[DEBUG - GPACONTEXTPROVIDER] currentGPA', currentGPA);
-  console.log('[DEBUG - GPACONTEXTPROVIDER] currentCredits', currentCredits);
-  console.log('[DEBUG - GPACONTEXTPROVIDER] modules', modules);
+  const editModule = (index: number, module: Module) => {
+    setModules((prev) => {
+      return prev.map((m, i) => {
+        if (i === index) {
+          return module;
+        }
+        return m;
+      });
+    });
+  };
+
+  // console.log('[DEBUG - GPACONTEXTPROVIDER] school', school);
+  // console.log('[DEBUG - GPACONTEXTPROVIDER] currentGPA', currentGPA);
+  // console.log('[DEBUG - GPACONTEXTPROVIDER] currentCredits', currentCredits);
+  // console.log('[DEBUG - GPACONTEXTPROVIDER] modules', modules);
 
   return (
-    <GPAContext.Provider value={{
-      school,
-      currentGPA,
-      currentCredits,
-      setSchool,
-      setCurrentGPA,
-      setCurrentCredits,
-      modules,
-      addModule,
-      removeModule,
-      targetGPA,
-      targetCredits,
-    }}>
+    <GPAContext.Provider
+      value={{
+        school,
+        currentGPA,
+        currentCredits,
+        setSchool,
+        setTargetGPA,
+        setTargetCredits,
+        setCurrentGPA,
+        setCurrentCredits,
+        modules,
+        addModule,
+        editModule,
+        removeModule,
+        targetGPA,
+        targetCredits,
+      }}
+    >
       {children}
     </GPAContext.Provider>
   );
