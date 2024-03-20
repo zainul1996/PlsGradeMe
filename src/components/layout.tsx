a732c3269f995457f5dc8ff568855a9c74b7fd6e
Zainul <zainul1996@gmail.com>
2024-03-21 03:33:22 +0800

    updated title for homepage

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -117,7 +117,7 @@ export default function Layout({ children, }: { children: React.ReactNode }) {
         <header className="py-10">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-slate-900 dark:text-slate-100">
-              Dashboard
+              The GPA Calculator
             </h1>
           </div>
         </header>
