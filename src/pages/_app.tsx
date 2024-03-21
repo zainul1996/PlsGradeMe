80c38dce04da964e2792aaeb57cce9fc4bbdb2c8
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-21 20:26:21 +0800

    feat(ui): add next-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,18 @@ import type { AppProps } from 'next/app';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import { GPAContextProvider } from '@components/contexts/GPAContextProvider';
+import { NextUIProvider } from '@nextui-org/system';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GPAContextProvider>
-        <Component {...pageProps} />
-      </GPAContextProvider>
+      <NextUIProvider>
+        <GPAContextProvider>
+          <main className="dark text-foreground bg-background">
+            <Component {...pageProps} />
+          </main>
+        </GPAContextProvider>
+      </NextUIProvider>
       <SpeedInsights />
       <Analytics />
     </>
