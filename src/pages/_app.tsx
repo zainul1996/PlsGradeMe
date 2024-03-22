cbb5c8d47bbe3fec75c0df26f02f8dd7ca1ef49d
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-22 13:05:10 +0800

    build(deps): remove next-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,13 @@ import type { AppProps } from 'next/app';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import { GPAContextProvider } from '@components/contexts/GPAContextProvider';
-import { NextUIProvider } from '@nextui-org/system';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NextUIProvider>
-        <GPAContextProvider>
-          <main className="dark text-foreground bg-background">
-            <Component {...pageProps} />
-          </main>
-        </GPAContextProvider>
-      </NextUIProvider>
+      <GPAContextProvider>
+        <Component {...pageProps} />
+      </GPAContextProvider>
       <SpeedInsights />
       <Analytics />
     </>
