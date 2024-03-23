0b524f43da66daede206d31003b8819bd827ced7
Zainul <zainul1996@gmail.com>
2024-03-24 04:48:59 +0800

    corrected styling for targetgpa tab

--- src/components/CalculateTargetGPATab.tsx
+++ src/components/CalculateTargetGPATab.tsx
@@ -86,57 +86,69 @@ export default function CalculateTargetGPATab({ school }: { school: School }) {
       <fieldset
         className={classNames(
           !school ? 'opacity-40' : 'opacity-100',
-          'rounded-md bg-white p-6 shadow-sm transition-all dark:bg-gray-800'
+          'rounded-md bg-white p-6 shadow-sm transition-all dark:bg-transparent'
         )}
       >
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <label className="block text-gray-700 dark:text-gray-200">
-            Target GPA
-            <input
-              disabled={!school}
-              type="text"
-              name="Current GPA"
-              id="current_gpa"
-              className="mt-1 block w-full rounded-md shadow-sm dark:bg-gray-700 dark:text-gray-200"
-              placeholder="3.25"
-              value={values.targetGPA || ''}
-              onChange={(e) =>
-                setFieldValue('targetGPA', e.target.value.trim())
-              }
-            />
-          </label>
+          <div className="flex-1">
+            <label
+              htmlFor="current_gpa"
+              className="block w-full border-0 p-0 text-gray-300 placeholder:text-gray-700 focus:ring-0 disabled:cursor-not-allowed sm:text-sm sm:leading-6"
+            >
+              Target GPA
+            </label>
+            <div className="relative rounded-md py-2 pl-3 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-indigo-600 dark:ring-slate-600 dark:focus-within:ring-slate-500">
+              <input
+                disabled={!school}
+                type="text"
+                name="Current GPA"
+                id="current_gpa"
+                className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 disabled:cursor-not-allowed sm:text-sm sm:leading-6 dark:bg-transparent dark:text-slate-200 dark:placeholder-slate-400"
+                placeholder="3.25"
+                value={values.targetGPA || ''}
+                onChange={(e) =>
+                  setFieldValue('targetGPA', e.target.value.trim())
+                }
+              />
+            </div>
+            {errors.targetGPA && (
+              <p className="text-xs text-red-600 dark:text-red-400">
+                {errors.targetGPA}
+              </p>
+            )}
+          </div>
 
-          {errors.targetGPA && (
-            <p className="text-xs text-red-600 dark:text-red-400">
-              {errors.targetGPA}
-            </p>
-          )}
-
-          <label className="block text-gray-700 dark:text-gray-200">
-            Credits for next semester
-            <input
-              disabled={!school}
-              type="text"
-              name="Current Credits"
-              id="current_credits"
-              className="mt-1 block w-full rounded-md shadow-sm dark:bg-gray-700 dark:text-gray-200"
-              placeholder="130"
-              value={values.targetCredits || ''}
-              onChange={(e) =>
-                setFieldValue('targetCredits', e.target.value.trim())
-              }
-            />
-          </label>
-
-          {errors.targetCredits && (
-            <p className="text-xs text-red-600 dark:text-red-400">
-              {errors.targetCredits}
-            </p>
-          )}
+          <div className="flex-1">
+            <label
+              htmlFor="current_credits"
+              className="block w-full border-0 p-0 text-gray-300 placeholder:text-gray-700 focus:ring-0 disabled:cursor-not-allowed sm:text-sm sm:leading-6"
+            >
+              Credits for next semester
+            </label>
+            <div className="relative rounded-md py-2 pl-3 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-indigo-600 dark:ring-slate-600 dark:focus-within:ring-slate-500">
+              <input
+                disabled={!school}
+                type="text"
+                name="Current Credits"
+                id="current_credits"
+                className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 disabled:cursor-not-allowed sm:text-sm sm:leading-6 dark:bg-transparent dark:text-slate-200 dark:placeholder-slate-400"
+                placeholder="130"
+                value={values.targetCredits || ''}
+                onChange={(e) =>
+                  setFieldValue('targetCredits', e.target.value.trim())
+                }
+              />
+            </div>
+            {errors.targetCredits && (
+              <p className="text-xs text-red-600 dark:text-red-400">
+                {errors.targetCredits}
+              </p>
+            )}
+          </div>
         </div>
       </fieldset>
 
-      {calculatedTargetGPA && values.targetCredits && values.targetCredits && (
+      {calculatedTargetGPA && values.targetCredits && (
         <div className={'my-3 text-center text-gray-200'}>
           {calculatedTargetGPA.isAchievable ? (
             <p>
