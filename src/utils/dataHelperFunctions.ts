acf251893ed84928b5f4c969888cb3d13c7a40ff
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-25 15:56:59 +0800

    refactor: add comment

--- src/utils/dataHelperFunctions.ts
+++ src/utils/dataHelperFunctions.ts
@@ -3,10 +3,20 @@
  */
 import { School } from '@components/contexts/GPAContextProvider';
 
+/**
+ * Get a grade by the points (e.g. 4.0 -> A+)
+ * @param school
+ * @param points
+ */
 export function getGradeByPoints(school: School, points: number) {
   return Object.entries(school.grades).find(([grade, p]) => p === points)?.[0];
 }
 
+/**
+ * Get the points by the grade (e.g. A+ -> 4.0)
+ * @param school
+ * @param grade
+ */
 export function getPointsByGrade(school: School, grade: string): number {
   return school.grades[grade as keyof School['grades']] || 0;
 }
