a5606a5410dd56832dc26a69c55ee95fcdb39bb7
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-25 15:43:02 +0800

    feat(data-store): update data store

--- src/pages/api/store.ts
+++ src/pages/api/store.ts
@@ -1,5 +1,6 @@
 import { kv } from '@vercel/kv';
 import { NextApiRequest, NextApiResponse } from 'next';
+import md5 from 'md5';
 
 export async function getHashData(req: NextApiRequest, res: NextApiResponse) {
   const { hash } = req.query;
@@ -12,18 +13,32 @@ export async function getHashData(req: NextApiRequest, res: NextApiResponse) {
   } catch (error) {
     res
       .status(500)
-      .json({ error: 'An error occurred while fetching the data' });
+      .json({ error: 'An error occurred while fetching the data', data: (error as Error).message });
   }
 }
 
 export async function storeHashData(req: NextApiRequest, res: NextApiResponse) {
-  const { hash } = req.query;
   const data = req.body;
+  const stringifiedData = JSON.stringify({...data, version: 1});
+  const hash = md5(stringifiedData)
+
 
   try {
-    await kv.set(hash as string, data);
-    res.json({ success: true });
+    await kv.set(hash, data);
+    res.json({ success: true, hash, data: stringifiedData});
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while storing the data' });
+    res.status(500).json({ error: 'An error occurred while storing the data', data: (error as Error).message });
   }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    return getHashData(req, res);
+  }
+
+  if (req.method === 'POST') {
+    return storeHashData(req, res);
+  }
+
+  res.status(405).json({ error: 'Method not allowed' });
 }
